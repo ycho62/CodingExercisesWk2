76802b93882fdf20e45aa946aa4c6cd27aebb34f
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-03-03 16:09:50 -0500

    Update solution.java

--- solution.java
+++ solution.java
@@ -1,9 +1,12 @@
 public class Solution {
   
   // use this method as a prototype for your answers...
-  void foo() {
-    // make a copy of this, call it soln1 (or soln2 etc...)
-    // and put your code here.
+  void soln1() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
   }
   
 }
