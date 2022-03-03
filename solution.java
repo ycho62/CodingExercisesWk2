fbb5ef5a30a9aa722789f581257dab2f9ccd1cbd
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-03-03 15:47:14 -0500

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+  
+  // use this method as a prototype for your answers...
+  void foo() {
+    // make a copy of this, call it soln1 (or soln2 etc...)
+    // and put your code here.
+  }
+  
+}
