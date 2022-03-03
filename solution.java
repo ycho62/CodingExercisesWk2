a2270e88dd48edd819656a612ac6b96d9384ed5e
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-03-03 16:11:41 -0500

    Update solution.java

--- solution.java
+++ solution.java
@@ -9,4 +9,84 @@ public class Solution {
   // and put your code here.
   }
   
+    void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln2() {
+  // and put your code here.
+  }
+  
+  void soln15() {
+  // and put your code here.
+  }
+  
+  void soln16() {
+  // and put your code here.
+  }
+  
+  void soln17() {
+  // and put your code here.
+  }
+  
+  void soln18() {
+  // and put your code here.
+  }
+  
+  void soln19() {
+  // and put your code here.
+  }
+  
+  void soln20() {
+  // and put your code here.
+  }
+  
+  void soln21() {
+  // and put your code here.
+  }
+  
+  void soln22() {
+  // and put your code here.
+  }
+  
 }
